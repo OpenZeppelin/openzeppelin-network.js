214234d6387964a5ff22ff6f375975603e52142f
Francisco Giordano <frangio.1@gmail.com>
2019-07-30 15:27:35 -0300

    make hooks return non-null objects

--- src/react/useWeb3Hook.ts
+++ src/react/useWeb3Hook.ts
@@ -1,41 +1,39 @@
+import { Provider } from 'web3/providers';
 // TODO: Add React and React Hook linting rules
-import { useState, useEffect, DependencyList } from 'react';
+import { useState, useEffect } from 'react';
+import * as providers from '../providers';
 
 import Web3Context, { Web3ContextOptions } from '../context/Web3Context';
 import { fromInjected, fromConnection } from '../context/factory';
 
 type Web3ContextCallback = () => Promise<Web3Context>;
 
-export function useWeb3Context(factory: Web3ContextCallback, deps?: DependencyList): Web3Context | null {
-  const [context, setContext] = useState<Web3Context | null>(null);
+export function useWeb3Context(provider: Provider, options?: Web3ContextOptions): Web3Context {
+  // TODO: update the context when the options change
+  const [context, setContext] = useState(() => new Web3Context(provider, options));
 
   useEffect(
     (): (() => void) => {
-      const getWeb3 = async (): Promise<void> => {
-        const web3Context = await factory();
-        setContext(web3Context);
-      };
-      getWeb3();
+      context.startPoll();
       return (): void => {
-        if (context) {
-          context.stopPoll();
-        }
+        context.stopPoll();
       };
     },
-    deps ? deps : [],
+    [context]
   );
 
   return context;
 }
 
-export function useWeb3Injected(options?: Web3ContextOptions, deps?: DependencyList): Web3Context | null {
-  return useWeb3Context((): Promise<Web3Context> => fromInjected(options), deps);
+export function useWeb3Injected(options?: Web3ContextOptions): Web3Context {
+  const [provider] = useState(() => providers.injected());
+  return useWeb3Context(provider, options);
 }
 
 export function useWeb3Network(
   connection: string,
-  options?: Web3ContextOptions,
-  deps?: DependencyList,
-): Web3Context | null {
-  return useWeb3Context((): Promise<Web3Context> => fromConnection(connection, options), deps);
+  options?: Web3ContextOptions
+): Web3Context {
+  const [provider] = useState(() => providers.connection(connection))
+  return useWeb3Context(provider, options);
 }
