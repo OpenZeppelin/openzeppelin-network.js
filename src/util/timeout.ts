953f1a0b6922658aa6807335e957df5d0948fba9
Francisco Giordano <frangio.1@gmail.com>
2019-07-31 15:40:02 -0300

    switch from Promise constructor to async function

--- src/util/timeout.ts
+++ src/util/timeout.ts
@@ -3,12 +3,10 @@ import sleep from './sleep';
 export default async function timeout<T>(target: Promise<T>, ms: number): Promise<T> {
   return Promise.race([
     target,
-    new Promise<T>(
-      async (resolve, reject): Promise<void> => {
+    (async () => {
         await sleep(ms);
         // TODO: Figure out how to print function name
-        reject(new Error(`Function has timed out with the limit of ${ms} milliseconds.`));
-      },
-    ),
+        throw new Error(`Function has timed out with the limit of ${ms} milliseconds.`);
+    })(),
   ]);
 }
