5e88d862fd4754a404e1f5edd5afdb652f36c6b0
Francisco Giordano <frangio.1@gmail.com>
2019-08-01 13:27:12 -0300

    remove forceUpdate

--- src/react/useWeb3Hook.ts
+++ src/react/useWeb3Hook.ts
@@ -1,6 +1,6 @@
 import { Provider } from 'web3/providers';
 // TODO: Add React and React Hook linting rules
-import { useState, useEffect, useReducer, Dispatch } from 'react';
+import { useState, useEffect } from 'react';
 import * as providers from '../providers';
 
 import Web3Context, { Web3ContextOptions } from '../context/Web3Context';
@@ -8,27 +8,9 @@ import { fromInjected, fromConnection } from '../context/factory';
 
 type Web3ContextCallback = () => Promise<Web3Context>;
 
-function useForceUpdate(): Dispatch<unknown> {
-  const [, forceUpdate] = useReducer((x, _: unknown) => x + 1, 0);
-  return forceUpdate;
-}
-
 export function useWeb3Context(provider: Provider, options?: Web3ContextOptions): Web3Context {
   // TODO: update the context when the options change
   const [context, setContext] = useState(() => new Web3Context(provider, options));
-  const forceUpdate = useForceUpdate();
-
-  // Causes components down the tree to re-render when any of the Web3Context properties change.
-  useEffect((): (() => void) => {
-    context.on('NetworkIdChanged', forceUpdate);
-    context.on('AccountsChanged', forceUpdate);
-    context.on('ConnectionChanged', forceUpdate);
-    return (): void => {
-      context.off('NetworkIdChanged', forceUpdate);
-      context.off('AccountsChanged', forceUpdate);
-      context.off('ConnectionChanged', forceUpdate);
-    };
-  }, [context]);
 
   useEffect((): (() => void) => {
     context.startPoll();
