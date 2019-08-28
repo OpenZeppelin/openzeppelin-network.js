92bd378c67c19de6165f55b800bbf0a24a148b9e
Igor Yalovoy <igor@yalovoi.net>
2019-08-28 22:57:35 +0300

    Move React to submodule

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,5 @@
 import Web3Context from './context/Web3Context';
-import { useWeb3Injected, useWeb3Network, useWeb3 } from './react/useWeb3Hook';
-import { useEphemeralKey } from './react/useEphemeralKeyHook';
 import { fromInjected, fromConnection } from './context/factory';
 import { ephemeral } from './wallet/keys';
 
-export {
-  Web3Context,
-  fromInjected,
-  fromConnection,
-  useWeb3Injected,
-  useWeb3Network,
-  useWeb3,
-  useEphemeralKey,
-  ephemeral,
-};
+export { Web3Context, fromInjected, fromConnection, ephemeral };
---
+++ src/react/index.ts
@@ -0,0 +1,4 @@
+import { useWeb3Injected, useWeb3Network, useWeb3 } from './useWeb3Hook';
+import { useEphemeralKey } from './useEphemeralKeyHook';
+
+export { useWeb3Injected, useWeb3Network, useWeb3, useEphemeralKey };
